02c7b0eeddd63baf71752bf31b84e96f675e2edf
olesia2929 <karpina.pn@ucu.edu.ua>
2025-04-30 18:18:35 +0300

    added solution to delete node in BST problem with a screenshot

--- sort_binary_tree_by_levels/sort_binary_tree_by_levels.py
+++ sort_binary_tree_by_levels/sort_binary_tree_by_levels.py
@@ -4,6 +4,13 @@
 #         self.right = R
 #         self.value = n
 from collections import deque
+
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+
 def tree_by_levels(root):
     if root is None:
         return []
