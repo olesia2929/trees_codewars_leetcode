802cb41c4495a56821cf35bfd78d5f3786fea5bd
olesia2929 <karpina.pn@ucu.edu.ua>
2025-04-30 17:30:30 +0300

    added solution to a sort binary tree by levels problem with a screenshot

---
+++ sort_binary_tree_by_levels/sort_binary_tree_by_levels.py
@@ -0,0 +1,19 @@
+# class Node:
+#     def __init__(self, L, R, n):
+#         self.left = L
+#         self.right = R
+#         self.value = n
+from collections import deque
+def tree_by_levels(root):
+    if root is None:
+        return []
+    result = []
+    queue = deque([root])
+    while queue:
+        current = queue.popleft()
+        result.append(current.value)
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    return result
